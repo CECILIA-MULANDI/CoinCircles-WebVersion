38d3b1841d0c13feba3ea874faae09416f3b1c34
cecilia mulandi <mulandicecilia4@gmail.com>
2024-04-23 16:39:41 +0300

    create-chama function

--- coincircles/contracts/hello.sol
+++ coincircles/contracts/hello.sol
@@ -2,32 +2,67 @@
 pragma solidity 0.8.19;
 
 contract CoinCircles{
+    // global variables
+    uint256 chamaIds;
     // user
     struct User{
-        // string email_address;
         address wallet_address;
-        bool isRegistered;
+        bool isConnected;
+    }
+    struct Chama{
+        string name;
+        string purpose;
+        uint256 maxNoOfPeople;
+        uint256 minDeposit;
+        bool isFull;
     }
     // mappings
     mapping (address=>User) public users;
-
+    mapping(uint256=>Chama) public chamas;
+    
     // events
-    event UserRegistered(address wallet_address);
+    event UserConnected(address wallet_address);
+    event ChamaCreated(uint256 chamaId,string name);
 
     // register function
-    function register_user(string memory email_address)   public {
+    function connect_user()   public {
         // check if the email is registered
-        require(!users[msg.sender].isRegistered, "User is already  registered");
+        require(!users[msg.sender].isConnected, "User is already  Connected");
 
         // create a new User
         User memory newUser =User({
           
             wallet_address:msg.sender,
-            isRegistered:true
+            isConnected:true
         });
         users[msg.sender]=newUser;
-        emit UserRegistered(msg.sender);
+        emit UserConnected(msg.sender);
 
+
+    }
+
+    // create chama function
+    function create_chama(string memory _name,string memory _purpose,uint256 _maxNoPeople,uint256 _minDeposit) public{
+        // check if the user is logged in
+        require(users[msg.sender].isConnected,"User is not connected");
+        // check if there is a chama with the same name
+        for (uint256 i =0;i<=chamaIds;i++){
+           require( keccak256(bytes(chamas[i].name)) != keccak256(bytes(_name)),"A chama with this name already exists");
+        }
+        
+        Chama memory new_chama=Chama({
+            name:_name,
+            purpose:_purpose,
+           maxNoOfPeople:_maxNoPeople,
+            minDeposit:_minDeposit,
+            isFull:false
+        });
+        chamas[chamaIds]=new_chama;
+        chamaIds++;
+        emit ChamaCreated(chamaIds, _name);
+        
+       
+        
 
     }
 }
